0be8484c0d34af290306a51c909f3c93c852be45
Suns <1024746628@qq.com>
2022-11-11 14:22:12 +0800

    编译altlr4+python环境

---
+++ 编译.py
@@ -0,0 +1,21 @@
+﻿import sys
+from antlr4 import *
+from HelloLexer import HelloLexer
+from HelloParser import HelloParser
+from HelloListener import HelloListener
+ 
+class HelloPrintListener(HelloListener):
+    def enterR(self, ctx):
+        # 函数名enterR的R指的是非终结符r
+        print("Hello: %s" % ctx.ID())
+def main():
+    lexer = HelloLexer(StdinStream())
+    stream = CommonTokenStream(lexer)
+    parser = HelloParser(stream)
+    tree = parser.r()
+    printer = HelloPrintListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
+ 
+if __name__ == '__main__':
+    main()
